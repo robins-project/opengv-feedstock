295fbcfb7623ebd6ecae4d0f45e35d71e51e7224
v-turin <vladimir.turin@opencascade.com>
2018-11-08 14:48:14 +0300

    Linux build

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,28 +1,31 @@
 #!/bin/sh
 mkdir build && cd build
 
-P=$(echo $PY_VER | tr "." "\n")
-PYVER=($P)
+# P=$(echo $PY_VER | tr "." "\n")
+# PYVER=($P)
 
-PY_MAJOR=${PYVER[0]}
-PY_MINOR=${PYVER[1]}
-if [ $PY3K -eq 1 ]; then
-    BUILD_FOR_PYTHON3=ON
-    BOOST_PYTHON_COMPONENT="-DBOOST_PYTHON3_COMPONENT=python${PY_MAJOR}${PY_MINOR}"
-    BOOST_NUMPY_COMPONENT="-DBOOST_NUMPY_COMPONENT=numpy${PY_MAJOR}${PY_MINOR}"
-else
-    BUILD_FOR_PYTHON3=OFF
-    BOOST_PYTHON_COMPONENT="-DBOOST_PYTHON_COMPONENT=python${PY_MAJOR}${PY_MINOR}"
-    BOOST_NUMPY_COMPONENT="-DBOOST_NUMPY_COMPONENT=numpy${PY_MAJOR}${PY_MINOR}"
-fi
+# PY_MAJOR=${PYVER[0]}
+# PY_MINOR=${PYVER[1]}
+# if [ $PY3K -eq 1 ]; then
+#     BUILD_FOR_PYTHON3=ON
+#     BOOST_PYTHON_COMPONENT="-DBOOST_PYTHON3_COMPONENT=python${PY_MAJOR}${PY_MINOR}"
+#     BOOST_NUMPY_COMPONENT="-DBOOST_NUMPY_COMPONENT=numpy${PY_MAJOR}${PY_MINOR}"
+# else
+#     BUILD_FOR_PYTHON3=OFF
+#     BOOST_PYTHON_COMPONENT="-DBOOST_PYTHON_COMPONENT=python${PY_MAJOR}${PY_MINOR}"
+#     BOOST_NUMPY_COMPONENT="-DBOOST_NUMPY_COMPONENT=numpy${PY_MAJOR}${PY_MINOR}"
+# fi
+
+    # -DBUILD_FOR_PYTHON3=${BUILD_FOR_PYTHON3}        \
+    # ${BOOST_PYTHON_COMPONENT}                       \
+    # ${BOOST_NUMPY_COMPONENT}                        \
 
 cmake -G "Ninja"                                    \
     -DCMAKE_INSTALL_PREFIX=${PREFIX}                \
+    -DCMAKE_PREFIX_PATH=${PREFIX}                   \
     -DCMAKE_BUILD_TYPE=Release                      \
     -DBUILD_PYTHON=ON                               \
-    -DBUILD_FOR_PYTHON3=${BUILD_FOR_PYTHON3}        \
-    ${BOOST_PYTHON_COMPONENT}                       \
-    ${BOOST_NUMPY_COMPONENT}                        \
+    -DPYTHON_EXECUTABLE=`which python`              \
     -DPYTHON_INSTALL_DIR=$SP_DIR                    \
     -DBUILD_TESTS=OFF                               \
     ..
